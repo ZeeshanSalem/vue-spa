9b1e543c2b87f5be1007d3401087b4a5a6c9b2f8
Zeeshan Saleem <zeeshansaleem729@gmail.com>
2023-11-01 08:19:41 +0400

    working

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,23 @@
 <template>
-    <p>this is the pages list</p>
-</template>
+    <div>
+      <!-- <p>{{ counter }}</p> -->
+      <p>{{ data.counter }}</p>
+      <button @click.prevent="click">click</button>
+    </div>
+  </template>
+  
+<script setup>
+import {ref, reactive} from 'vue';
+
+// const counter  = ref(0);
+
+const data = reactive({
+    counter : 0,
+});
+
+function click(){
+
+    // counter.value++;
+    data.counter++;
+}
+</script>
